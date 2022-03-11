0a2207500f912ba1eb594115095f13198e4c67b4
Kolanu Harish <kolanu.harish@ibm.com>
2022-03-11 22:59:03 +0530

    updated files

--- deploy_db_changes.sh
+++ deploy_db_changes.sh
@@ -14,7 +14,7 @@ end_color="\e[0m"
 
 
 ## Get the updated sql files
-git_changes=($(git diff ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}..${GIT_COMMIT} --name-only | grep *.sql))
+git_changes=($(git diff ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}..${GIT_COMMIT} --name-only | grep "\.sql$"))
 
 if [ -z ${git_changes} ]; then
     echo -e "${yellow}Warning: There is no DB changes for this build.${end_color}"
