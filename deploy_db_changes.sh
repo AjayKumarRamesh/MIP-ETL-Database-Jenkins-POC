b7b7b74dc14bda3961aa780c2594f4d542d53284
Kolanu Harish <kolanu.harish@ibm.com>
2022-03-11 23:11:49 +0530

    updated script

--- deploy_db_changes.sh
+++ deploy_db_changes.sh
@@ -9,6 +9,7 @@ db_password='IbmDB2#12345678'
 red="\e[1;31m"
 green="\e[1;32m"
 yellow="\e[1;33m"
+blue="\e[1;34m"
 end_color="\e[0m"
 
 
@@ -19,9 +20,10 @@ git_changes=($(git diff ${GIT_PREVIOUS_SUCCESSFUL_COMMIT}..${GIT_COMMIT} --name-
 if [ -z ${git_changes} ]; then
     echo -e "${yellow}Warning: There is no DB changes for this build.${end_color}"
 else
+    echo -e "\nBelow are the updated sql files.\n${git_changes}\n"
     for each_change in ${git_changes[@]}; do
-        echo ${each_change}
-        liquibase status --changelog-file=${each_change}
+        echo "${blue}--------- Deploying ${each_change} ---------${end_color}"
+        liquibase update --changelog-file=${each_change}
     done
 fi 
 
