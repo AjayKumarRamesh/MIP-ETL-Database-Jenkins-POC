7217b8b60ce651fc1b6bf02db295e10a958870fd
Kolanu Harish <kolanu.harish@ibm.com>
2022-03-17 15:23:31 +0530

    updated sql file

--- DatabaseObjects/DDLs/insert_store_table_values.sql
+++ DatabaseObjects/DDLs/insert_store_table_values.sql
@@ -4,3 +4,9 @@
 INSERT INTO LIQ_SCHEMA.stores (store_id, store_name, city_id)
 VALUES (10, 'IBM_Bangalore', 111);
 -- rollback DELETE FROM LIQ_SCHEMA.STORES WHERE store_id=10;
+
+
+-- changeset harish:insert_store_table_values_2
+INSERT INTO LIQ_SCHEMA.stores (store_id, store_name, city_id)
+VALUES (20, 'IBM_Hyderabad', 222);
+-- rollback DELETE FROM LIQ_SCHEMA.STORES WHERE store_id=20;
